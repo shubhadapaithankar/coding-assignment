fd71ebafee9ff6016253cb131e4afe16d03a7f03
shubhadapaithankar <shubhadasanjay.paithankar@sjsu.edu>
2023-05-24 13:11:21 -0700

    code has been added

---
+++ test.py
@@ -0,0 +1,31 @@
+
+/* Player Styles */
+
+.player-color {
+  background-color: #4a8fd4;
+}
+
+.player-area {
+  background-color: #265487;
+  background-image: url(images/chip.svg);
+}
+
+.player-card .power {
+  background-image: url(images/shield.svg);
+}
+
+/* Hacker Styles */
+
+.hacker-color {
+  background-color: #d44a4a;
+}
+
+.hacker-area {
+  background-color: #872626;
+  background-image: url(images/chip.svg);
+}
+
+.hacker-card .power {
+  background-image: url(images/skull.svg);
+}
+
